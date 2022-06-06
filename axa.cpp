70054ddbdcfd4c58510ccc880bfab38de4069aef
thpndx32 <erick.yari@ucsp.edu.pe>
2022-06-05 22:04:20 -0500

    name change

--- axa.cpp
+++ axa.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <iterator>
 using namespace std;
 
-void suma_backtrack(int n, vector<int> * vector) {
-    
+
+
+bool suma_backtrack(int n, vector<int> * vec, vector<int> *&aux) {
+    if (n==0) {
+        vector<int>::iterator it1 = aux->begin();
+        for (;it1<aux->end();it1++) {
+            cout << *it1<<" ";
+        }
+        cout << endl;
+        return true;
+    }
+    vector<int>::iterator it = vec->begin();
+    for (;it<vec->end();it++) {
+        aux->push_back(*it);
+        if(n-*it<0) {
+            aux->pop_back();
+            continue;
+        }
+        if (suma_backtrack(n-*it, vec, aux)) {
+            aux->pop_back();
+        }
+    }
+    aux->pop_back();
+    return false;
 }
 
 int main(){
-    cout<<"me llega al pincho csmr";
+    int x = 15;
+    vector<int> vec = {10,8,7,6,5,2};
+    vector<int> *aux = new vector<int>; 
+    suma_backtrack(x, &vec, aux);
 }
